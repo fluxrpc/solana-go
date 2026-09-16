e4b0d3e6436ca17c0018c6fb6a9cea683c1e5c46
hxuan190 <hxuan190@gmail.com>
2026-09-16 13:48:01 +0700

    feat(address-lookup-table): decode table accounts

--- programs/address-lookup-table/errors.go
+++ programs/address-lookup-table/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrUnknownInstruction = errors.New("addresslookup: unknown instruction")
 	ErrTooManyAddresses   = errors.New("addresslookup: too many addresses")
+	ErrInvalidAccountSize = errors.New("addresslookup: invalid account size")
 )
